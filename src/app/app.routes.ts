dd0956b151609dbbb48b39f015520dafe6bca43a
montheooo <montheooo@yahoo.fr>
2024-02-17 12:41:30 -0500

    add Home Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ClientListComponent } from './pages/client-list/client-list.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+
+];
