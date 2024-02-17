02253ec38e8044438e5513ef819706f8a83d1e9a
montheooo <montheooo@yahoo.fr>
2024-02-17 12:48:46 -0500

    add Invoices list component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { ClientListComponent } from './pages/client-list/client-list.component';
 import { HomeComponent } from './pages/home/home.component';
+import { InvoicesListComponent } from './pages/invoices-list/invoices-list.component';
 
 export const routes: Routes = [
 
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'invoices-list', component: InvoicesListComponent },
 
 ];
