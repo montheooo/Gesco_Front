971185ca3ac27e85914833e21731562f0482857b
montheooo <montheooo@yahoo.fr>
2024-02-18 18:55:21 -0500

    Add pages Production, Vente, Stock, Rapport

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,19 @@ import { Routes } from '@angular/router';
 import { ClientListComponent } from './pages/client-list/client-list.component';
 import { HomeComponent } from './pages/home/home.component';
 import { InvoicesListComponent } from './pages/invoices-list/invoices-list.component';
+import { ProductionComponent } from './pages/production/production.component';
+import { VenteComponent } from './pages/vente/vente.component';
+import { RapportComponent } from './pages/rapport/rapport.component';
+import { StockComponent } from './pages/stock/stock.component';
 
 export const routes: Routes = [
 
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'invoices-list', component: InvoicesListComponent },
+  { path: 'production', component: ProductionComponent },
+  { path: 'vente', component: VenteComponent },
+  { path: 'rapport', component: RapportComponent },
+  { path: 'stock', component: StockComponent },
 
 ];
