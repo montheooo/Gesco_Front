cd60c2ccdacde4d840bcfd37491c2cea869b2dd7
montheooo <montheooo@yahoo.fr>
2024-09-29 11:50:48 -0400

    add invoice fornissuer component

---
+++ src/app/pages/invoices-fournisseur-list/invoices-fournisseur-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-invoices-fournisseur-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './invoices-fournisseur-list.component.html',
+  styleUrl: './invoices-fournisseur-list.component.css'
+})
+export class InvoicesFournisseurListComponent {
+
+}
